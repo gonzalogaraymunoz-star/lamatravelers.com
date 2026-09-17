8eafcfdd3271350d009e18932bca0549817648d3
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-17 02:52:18 -0300

    brand: use complete transparent LAMA logo for favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,13 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import WhatsAppButton from '@/components/WhatsAppButton';
 
+const fullLogoIcon = 'https://drive.google.com/thumbnail?id=1dYFHFgum_0EMYSCCBYuhrRQjTgFRHaMb&sz=w512';
+
 export const metadata: Metadata = {
   title: { default: 'LAMA Travelers | Experiencias en San Pedro de Atacama', template: '%s | LAMA Travelers' },
-  description: 'Expediciones, tours, montaña, astronomía, wellness y transfers en San Pedro de Atacama. Diseñamos tu experiencia en Atacama.',
+  description: 'Expediciones, tours, montaña y astronomía en San Pedro de Atacama. Diseñamos tu experiencia en Atacama.',
   metadataBase: new URL('https://lamatravelers.com'),
-  icons: { icon: '/icon.svg', shortcut: '/icon.svg', apple: '/icon.svg' },
+  icons: { icon: fullLogoIcon, shortcut: fullLogoIcon, apple: fullLogoIcon },
   openGraph: { title:'LAMA Travelers', description:'Atacama, hecho a tu medida.', type:'website', locale:'es_CL' }
 };
 
